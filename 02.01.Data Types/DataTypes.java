9476c63da6cd4bb9d59e47f151f1f47dd75681b6
Nikhil <ace.nikchauhan@gmail.com>
2024-03-18 22:32:42 +0530

    added variables, data types and literals

---
+++ 02.01.Data Types/DataTypes.java
@@ -0,0 +1,18 @@
+public class DataTypes {
+    
+    public static void main(String args[]){
+        int num1 = 10;
+        byte by = 127;
+        short sh = 500;
+        long l = 542526l;
+
+        float f = 5.8f;
+        double d = 5.94;
+
+        char c = 'N';
+
+        boolean b = true;
+
+        
+    }
+}
--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 <img src="https://res.cloudinary.com/dxwqpwwrh/image/upload/v1710778444/java/Screenshot_2024-03-18_214130_us2slz.png">
 
-<h3>First Program</h3>
+<h4>01.First Program</h4>
+<h4>02.00.Variables</h4>
+<ul>
+    <li>02.01.Data Types</li>
+    <li>02.02.Literals</li>
+    <li>02.03</li>
+</ul>
